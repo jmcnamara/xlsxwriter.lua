fbb8cd58c70211b9f203ca54090c0831cccb08ee
John McNamara <jmcnamara@cpan.org>
2014-03-12 20:02:34 +0000

    Added App class.

---
+++ test/app/test_app02.lua
@@ -0,0 +1,72 @@
+----
+-- Tests for the xlsxwriter.lua app class.
+--
+-- Copyright 2014, John McNamara, jmcnamara@cpan.org
+--
+
+require "Test.More"
+require "Test.LongString"
+
+plan(1)
+
+----
+-- Tests setup.
+--
+local expected
+local got
+local caption
+local App = require "xlsxwriter.app"
+local app
+
+-- Remove extra whitespace in the formatted XML strings.
+function _clean_xml_string(s)
+  return (string.gsub(s, ">%s+<", "><"))
+end
+
+----
+-- Test the _write_app() method.
+--
+caption = " \tApp: App: _assemble_xml_file()"
+expected = _clean_xml_string([[
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">
+  <Application>Microsoft Excel</Application>
+  <DocSecurity>0</DocSecurity>
+  <ScaleCrop>false</ScaleCrop>
+  <HeadingPairs>
+    <vt:vector size="2" baseType="variant">
+      <vt:variant>
+        <vt:lpstr>Worksheets</vt:lpstr>
+      </vt:variant>
+      <vt:variant>
+        <vt:i4>2</vt:i4>
+      </vt:variant>
+    </vt:vector>
+  </HeadingPairs>
+  <TitlesOfParts>
+    <vt:vector size="2" baseType="lpstr">
+      <vt:lpstr>Sheet1</vt:lpstr>
+      <vt:lpstr>Sheet2</vt:lpstr>
+    </vt:vector>
+  </TitlesOfParts>
+  <Company>
+  </Company>
+  <LinksUpToDate>false</LinksUpToDate>
+  <SharedDoc>false</SharedDoc>
+  <HyperlinksChanged>false</HyperlinksChanged>
+  <AppVersion>12.0000</AppVersion>
+</Properties>]])
+
+app = App:new()
+app:_set_filehandle(io.tmpfile())
+
+app:_add_part_name('Sheet1')
+app:_add_part_name('Sheet2')
+app:_add_heading_pair('Worksheets', 2)
+
+
+app:_assemble_xml_file()
+
+got = _clean_xml_string(app:_get_data())
+
+is_string(got, expected, caption)
