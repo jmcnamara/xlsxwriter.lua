af912334a84a38c04cabd5ae16b193fc390535a2
John McNamara <jmcnamara@cpan.org>
2014-04-02 00:35:42 +0100

    Added more tests for print_area().

---
+++ test/comparison/lua/test_print_area07.lua
@@ -0,0 +1,18 @@
+----
+-- Test cases for xlsxwriter.lua.
+--
+-- Test worksheet print_area.
+--
+-- Copyright 2014, John McNamara, jmcnamara@cpan.org
+--
+
+local Workbook = require "xlsxwriter.workbook"
+
+local workbook  = Workbook:new("test_print_area07.xlsx")
+local worksheet = workbook:add_worksheet()
+
+worksheet:print_area("A1:XFD1048576")
+
+worksheet:write_string("A1", "Foo")
+
+workbook:close()
